c42138eb9b34f8f2f79dad5f589e6fa2044869fa
Wang Xuerui <wangxuerui2003@gmail.com>
2022-08-06 13:39:43 +0800

    delete unused functions, compile with different BUFFER_SIZE

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: wxuerui <wxuerui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 16:40:47 by wxuerui           #+#    #+#             */
-/*   Updated: 2022/07/31 16:12:21 by wxuerui          ###   ########.fr       */
+/*   Updated: 2022/08/06 13:22:22 by wxuerui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,65 +81,3 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	return (NULL);
 }
 
-char	*ft_itoa(int n)
-{
-	char	*res;
-	int		len;
-	long	temp;
-
-	len = n <= 0;
-	temp = n;
-	while (n && ++len)
-		n /= 10;
-	res = malloc((len + 1) * sizeof(char));
-	if (!res)
-		return (NULL);
-	res[len] = 0;
-	if (!temp)
-		res[0] = 48;
-	if (temp < 0)
-	{
-		res[0] = '-';
-		temp *= -1;
-	}
-	while (len - 1 >= 0 && temp && len--)
-	{
-		res[len] = temp % 10 + 48;
-		temp /= 10;
-	}
-	return (res);
-}
-
-static size_t	ft_strlen(char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t	s1_len;
-	size_t	s2_len;
-	char	*res;
-	size_t	i;
-
-	if (!s1 || !s2)
-		return (NULL);
-	s1_len = ft_strlen((char *)s1);
-	s2_len = ft_strlen((char *)s2);
-	res = malloc((s1_len + s2_len + 1) * sizeof(char));
-	if (!res)
-		return (NULL);
-	i = -1;
-	while (++i < s1_len)
-		res[i] = s1[i];
-	i -= 1;
-	while (++i < s1_len + s2_len)
-		res[i] = s2[i - s1_len];
-	res[i] = 0;
-	return (res);
-}
